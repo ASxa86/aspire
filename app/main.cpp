98500c9c87ad664cbab6bffaa1d38bcac78db07c
Aaron Shelley <amshelley86@gmail.com>
2024-02-14 21:28:54 -0500

    Successfully rendering window using WIN32.

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,14 @@
-#include <aspire/core/Signal.h>
+#include <aspire/core/Kernel.h>
+#include <aspire/gui/Window.h>
 
 int main()
 {
-	aspire::Signal<void(int)> test;
-	test.emit(5);
-	return 0;
+	aspire::Kernel kernel{};
+
+	auto* window = kernel.addChild<aspire::Window>();
+	window->setWidth(1280);
+	window->setHeight(720);
+	window->setTitle("Aspire");
+
+	return kernel.run();
 }
