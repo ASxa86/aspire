ff24b5a47806434dfecf2c7562f6f8b4bf3c7c52
Aaron Shelley <amshelley86@gmail.com>
2024-03-23 15:07:14 -0400

    Fix compilation errors for android architecture.

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -12,3 +12,6 @@ fi
 
 echo "mono ./vcpkg/downloads/tools/nuget-6.2.1-linux/nuget.exe sources add -source $NUGET_SOURCE -name $NUGET_NAME -username $GITLAB_USER -password $GITLAB_USER_TOKEN"
 mono ./vcpkg/downloads/tools/nuget-6.2.1-linux/nuget.exe sources add -source $NUGET_SOURCE -name $NUGET_NAME -username $GITLAB_USER -password $GITLAB_USER_TOKEN
+
+echo "mono ./vcpkg/downloads/tools/nuget-6.2.1-linux/nuget.exe config -set maxHttpRequestsPerSource=64"
+mono ./vcpkg/downloads/tools/nuget-6.2.1-linux/nuget.exe config -set maxHttpRequestsPerSource=64
--- triplets/arm64-android.cmake
+++ triplets/arm64-android.cmake
@@ -1,10 +1,21 @@
+set(STATIC_PORTS
+    harfbuzz
+    libb2
+)
+
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-# Qt only supports -DANDROID_STL=c++_shared
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+
+if(PORT IN_LIST STATIC_PORTS)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
+set(VCPKG_BUILD_TYPE release)
 
 # Qt requires that the ANDROID_SDK_ROOT is defined when compiling.
 set(ANDROID_SDK_ROOT "$ENV{ANDROID_HOME}")
