53cac5fb94ba424f826db7298b0ca1b04abd31e4
Aaron Shelley <amshelley86@gmail.com>
2024-04-06 21:19:22 +0000

    Implement android x64 support.

--- triplets/x64-android.cmake
+++ triplets/x64-android.cmake
@@ -10,11 +10,11 @@ else()
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
-set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
-set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86_64)
 set(VCPKG_BUILD_TYPE release)
 
 # Qt requires that the ANDROID_SDK_ROOT is defined when compiling.
