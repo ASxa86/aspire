1d93551ebf3150ac976d4094f503d3976c10a414
Aaron Shelley <amshelley86@gmail.com>
2024-04-23 23:15:56 -0400

    Remove Qt.

---
+++ include/aspire/core/Object.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <aspire/core/export.hxx>
+#include <memory>
+#include <string_view>
+#include <vector>
+
+namespace aspire::core
+{
+	class ASPIRE_CORE_EXPORT Object
+	{
+	public:
+		Object() = default;
+		virtual ~Object() = default;
+
+		Object(const Object&) = delete;
+		auto operator=(const Object&) -> Object& = delete;
+
+		Object(Object&&) noexcept = delete;
+		auto operator=(Object&&) noexcept -> Object& = delete;
+
+		auto setName(std::string_view x) -> void;
+		auto getName() const -> std::string_view;
+
+	private:
+		std::vector<std::unique_ptr<Object>> children;
+		std::string name;
+	};
+}
