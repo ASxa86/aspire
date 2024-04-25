f8f084bf856087168abd14ae06fe51f93f3b6641
Aaron Shelley <amshelley86@gmail.com>
2024-04-25 01:48:45 +0000

    Implement a point class.

---
+++ include/aspire/scene/Node.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <aspire/scene/export.hxx>
+
+namespace aspire::scene
+{
+	class ASPIRE_SCENE_EXPORT Node : public aspire::core::Object
+	{
+	public:
+		auto setScale() private:
+	};
+}
