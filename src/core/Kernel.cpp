2008fa262bdbe02c1dd22abb5328341dcb4f1dcf
Aaron Shelley <amshelley86@gmail.com>
2024-06-05 18:06:33 -0400

    Add frame-time recording on application exit.

--- src/core/Kernel.cpp
+++ src/core/Kernel.cpp
@@ -2,7 +2,9 @@
 
 #include <aspire/core/PimplImpl.h>
 #include <chrono>
+#include <iostream>
 #include <mutex>
+#include <numeric>
 
 using aspire::core::Kernel;
 using aspire::core::Service;
@@ -46,8 +48,12 @@ auto Kernel::run() -> int
 	this->pimpl->running = true;
 	this->pimpl->start = std::chrono::steady_clock::now();
 
+	std::vector<std::chrono::steady_clock::duration> frames;
+
 	while(this->pimpl->running)
 	{
+		const auto frameStart = std::chrono::steady_clock::now();
+
 		this->pimpl->elapsed += std::chrono::steady_clock::now() - this->pimpl->start;
 
 		{
@@ -63,7 +69,18 @@ auto Kernel::run() -> int
 		{
 			service->frame();
 		}
+
+		frames.emplace_back(std::chrono::steady_clock::now() - frameStart);
 	}
+
+	std::cout << "Frames: " << frames.size() << "\n";
+
+	const auto sum = std::accumulate(std::begin(frames), std::end(frames), std::chrono::steady_clock::duration::zero());
+	const auto avg = std::chrono::duration_cast<std::chrono::duration<double>>(sum).count() / static_cast<double>(frames.size());
+	const auto fps = 1.0 / avg;
+
+	std::cout << "Interval: " << avg << " s\n";
+	std::cout << "FPS: " << fps << " Hz\n";
 
 	return EXIT_SUCCESS;
 }
