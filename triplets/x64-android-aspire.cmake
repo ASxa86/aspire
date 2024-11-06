f8f9e4f575606e3798fbb186e3fc8852b463be3f
Aaron Shelley <amshelley86@gmail.com>
2024-11-06 09:13:38 -0500

    Begin adding support for android.

--- triplets/x64-android-aspire.cmake
+++ triplets/x64-android-aspire.cmake
@@ -1,14 +1,9 @@
-set(VCPKG_LIBRARY_LINKAGE static)
-
-if(${PORT} MATCHES "qt")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-android")
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86_64)
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=x86_64 -DANDROID_STL=c++_shared)
 set(VCPKG_BUILD_TYPE release)
 
 # Qt requires that the ANDROID_SDK_ROOT is defined when compiling.
